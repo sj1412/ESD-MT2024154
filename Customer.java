9162989d900b56b5bafb927f29d6f82d6ba9e14f
Soumya@14 <89511287+sj1412@users.noreply.github.com>
2024-11-20 23:47:41 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,38 @@
+package com.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+    @Column(name = "address", nullable = true)
+    private String address;
+
+    @Column(name = "city", nullable = true)
+    private String city;
+
+    @Column(name = "pincode", nullable = true)
+    private String pincode;
+
+}
